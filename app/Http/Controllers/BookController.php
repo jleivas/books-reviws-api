d8820f19dfd82cd310ac9277a158560124b1ed4e
Jorge Leiva <softdirex@gmail.com>
2018-12-13 16:33:28 -0300

    pudate

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -40,7 +40,7 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Book  $book
      * @return \Illuminate\Http\Response
      */
     public function show(Book $book)
